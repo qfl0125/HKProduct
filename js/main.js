620064c1bbe79d0992414b11e99dd72c0b458638
Perry <perry.qiu@qq.com>
2019-11-01 22:36:27 +0800

    update

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ $(document).ready(function($) {
 
 });
  function getList(product){
-        var item="<div class='item'><a class='example-image-link' href='images/pd/xhx.png' data-lightbox='example-set' data-title='Click the right half of the image to move forward.'><img class='example-image' src='images/pd/xhx.png' alt='/></a><div class='content-item'><span class='time'><a class='btn btn-1'>价格</a> <i class='fa fa-jpy' aria-hidden='true'></i>528</span><h3 class='title-item'><a href='#'>绣花绣水乳套装</a></h3><p class='info'>干皮敏感肌可用,巨好吸收啊,可以提亮肤色</p><ul class='list-inline'><li><a href='#'><i class='fa fa-comment'></i> 260</a></li><li><a href='#'><i class='fa fa-heart'></i> 260</a></li></ul></div>";
+        var item="<div class='item'> <a class='example-image-link' href='images/pd/xhx.png' data-lightbox='example-set' data-title='干皮敏感肌可用,巨好吸收啊,可以提亮肤色.'><img class='example-image' src='images/pd/xhx.png' alt=''/></a><div class='content-item'><span class='time'><a class='btn btn-1'>价格</a> <i class='fa fa-jpy' aria-hidden='true'></i>528</span><h3 class='title-item'><a href='#'>绣花绣水乳套装</a></h3><p class='info'>干皮敏感肌可用,巨好吸收啊,可以提亮肤色</p><ul class='list-inline'><li><a href='#'><i class='fa fa-comment'></i> 260</a></li><li><a href='#'><i class='fa fa-heart'></i> 260</a></li></ul></div>";
 
         var list=new Array();
         list.push(item);
@@ -27,5 +27,4 @@ $(document).ready(function($) {
           list.push(item);
            list.push(item);
         return list;
-      console.log("I LOVE YOU!");
 }
