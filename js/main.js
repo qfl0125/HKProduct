2dd911714189cb1cb5b7d295bb48d198991a6666
Perry <perry.qiu@qq.com>
2019-11-01 22:30:14 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function($) {
     "use strict";
-    
-    $('#container').pinto({
+    $('#container').append(getList).pinto({
         itemWidth:230,
         gapX:10,
         gapY:10,
         onItemLayout: function($item, column, position) {
         }
     });
+
     
     $("#pintoInit").click(function(){
         $('#container').pinto();
@@ -16,4 +16,16 @@ $(document).ready(function($) {
     $("#pintoDestroy").click(function(){
         $('#container').pinto("destroy");
     });
+
 });
+ function getList(product){
+        var item="<div class='item'><a class='example-image-link' href='images/pd/xhx.png' data-lightbox='example-set' data-title='Click the right half of the image to move forward.'><img class='example-image' src='images/pd/xhx.png' alt='/></a><div class='content-item'><span class='time'><a class='btn btn-1'>价格</a> <i class='fa fa-jpy' aria-hidden='true'></i>528</span><h3 class='title-item'><a href='#'>绣花绣水乳套装</a></h3><p class='info'>干皮敏感肌可用,巨好吸收啊,可以提亮肤色</p><ul class='list-inline'><li><a href='#'><i class='fa fa-comment'></i> 260</a></li><li><a href='#'><i class='fa fa-heart'></i> 260</a></li></ul></div>";
+
+        var list=new Array();
+        list.push(item);
+         list.push(item);
+          list.push(item);
+           list.push(item);
+        return list;
+      console.log("I LOVE YOU!");
+}
