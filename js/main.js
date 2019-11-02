2824cd8d6f2bf2b649986db0f64f77c554fce751
Perry <perry.qiu@qq.com>
2019-11-02 11:53:35 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -1,46 +1,52 @@
 $(document).ready(function($) {
     "use strict";
-    $('#container').append(jsonToObject()).pinto({
-        itemWidth:230,
-        gapX:10,
-        gapY:10,
-        onItemLayout: function($item, column, position) {
-        }
-    });
-
+    initDom($);
     
     $("#pintoInit").click(function(){
         $('#container').pinto();
     });
-    
+
     $("#pintoDestroy").click(function(){
         $('#container').pinto("destroy");
     });
 
 });
- function objToDom(item){
-        var item="<div class='item'>\
-             <a class='example-image-link' href=images/pd/"+item.image+" data-lightbox='example-set' data-title="+item.description+">\
-               <img class='example-image' src=images/pd/"+item.image+" alt=''/>\
-            </a>\
-            <div class='content-item'><span class='time'>\
-                <a class='btn btn-1'>价格</a>\
-                <i class='fa fa-jpy' aria-hidden='true'></i>"+item.price+"</span><h3 class='title-item'>\
-                <a href='#'>"+item.name+"</a></h3><p class='info'>"+item.description+"</p>\
-                <ul class='list-inline'>\
-                     <li><a href='#'><i class='fa fa-comment'></i>"+item.meg+"</a></li>\
-                     <li><a href='#'><i class='fa fa-heart'></i> "+item.heart+"</a></li>\
-                </ul>\
+
+function objToDom(item){
+            var dom="\
+            <div class=\'item\'>\
+                <a class=\'example-image-link\' href=\'images/pd/"+item.image+"\' data-lightbox=\'example-set\'\
+                 data-title=\'"+item.description+"\'>\
+                    <img class=\'example-image\' src=\'images/pd/"+item.image+"\' alt=\'\'/>\
+                </a>\
+                <div class=\'content-item\'><span class=\'time\'>\
+                    <a class=\'btn btn-1\'>价格</a> <i class=\'fa fa-jpy\' aria-hidden=\'true\'></i>"+item.price+"</span>\
+                    <h3 class=\'title-item\'><a href=\'#\'>"+item.name+"</a></h3>\
+                    <p class=\'info\'>"+item.description+"</p>\
+                    <ul class=\'list-inline\'>\
+                        <li><a href=\'#\'><i class=\'fa fa-comment\'></i>"+item.meg+"</a></li>\
+                        <li><a href=\'#\'><i class=\'fa fa-heart\'></i>"+item.heart+"</a></li>\
+                    </ul>\
             </div>";
-       console.log()
-       return item;
+       return dom;
 }
 
-function jsonToObject() {
-    $.getJSON("../product/list.json",function (data) {
+function initDom($) {
+    $.getJSON("https://qfl0125.github.io/HKProduct/product/list.json",function (json,status) {
         var listData=new Array();
-        console.log("jsonToObject----data:"+data)
-        listData.push(data.forEach(objToDom(item)));
-        return listData;
-    })
+        for (var i = 0; i < json.length; i++) {
+            listData.push(objToDom(json[i]));
+        }
+        //
+       $('#container').append(listData).pinto({
+        itemWidth:230,
+        gapX:10,
+        gapY:10,
+        onItemLayout: function($item, column, position) {
+            
+        }
+    });
+        //
+    });
 }
+ 
